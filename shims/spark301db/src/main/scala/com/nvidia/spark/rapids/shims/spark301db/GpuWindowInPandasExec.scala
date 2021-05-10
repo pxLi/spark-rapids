c157ab061b35903f9e1b3602fa02cf81c726f2d1
Peixin Li <pxli@nyu.edu>
2021-05-10 11:24:41 +0800

    add debug logging

--- shims/spark301db/src/main/scala/com/nvidia/spark/rapids/shims/spark301db/GpuWindowInPandasExec.scala
+++ shims/spark301db/src/main/scala/com/nvidia/spark/rapids/shims/spark301db/GpuWindowInPandasExec.scala
@@ -58,6 +58,7 @@ case class GpuWindowInPandasExec(
     val allExpressions = windowFramesWithExpressions.map(_._2).flatten
     val references = allExpressions.zipWithIndex.map { case (e, i) =>
       // Results of window expressions will be on the right side of child's output
+      logWarning("<<<<<<<========", e.toString(), i.toString, child.output.size + i, e.dataType, e.nullable)
       GpuBoundReference(child.output.size + i, e.dataType, e.nullable)
     }
     val unboundToRefMap = allExpressions.zip(references).toMap
