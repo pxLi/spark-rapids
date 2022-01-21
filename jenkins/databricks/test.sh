5c8b61002cd0912a34b415052f977a4fad56272c
Peixin Li <pxli@nyu.edu>
2022-01-21 15:57:39 +0800

    run window_function_test only

--- jenkins/databricks/test.sh
+++ jenkins/databricks/test.sh
@@ -80,7 +80,7 @@ export TEST_PARALLEL=4
 if [ -d "$LOCAL_JAR_PATH" ]; then
     if [[ $TEST_MODE == "ALL" || $TEST_MODE == "IT_ONLY" ]]; then
         ## Run tests with jars in the LOCAL_JAR_PATH dir downloading from the dependency repo
-        LOCAL_JAR_PATH=$LOCAL_JAR_PATH bash $LOCAL_JAR_PATH/integration_tests/run_pyspark_from_build.sh  --runtime_env="databricks" --test_type=$TEST_TYPE
+        LOCAL_JAR_PATH=$LOCAL_JAR_PATH bash $LOCAL_JAR_PATH/integration_tests/run_pyspark_from_build.sh  --runtime_env="databricks" --test_type=$TEST_TYPE -k window_function_test
 
         ## Run cache tests
         if [[ "$IS_SPARK_311_OR_LATER" -eq "1" ]]; then
@@ -98,7 +98,7 @@ if [ -d "$LOCAL_JAR_PATH" ]; then
 else
     if [[ $TEST_MODE == "ALL" || $TEST_MODE == "IT_ONLY" ]]; then
         ## Run tests with jars building from the spark-rapids source code
-        bash /home/ubuntu/spark-rapids/integration_tests/run_pyspark_from_build.sh --runtime_env="databricks" --test_type=$TEST_TYPE
+        bash /home/ubuntu/spark-rapids/integration_tests/run_pyspark_from_build.sh --runtime_env="databricks" --test_type=$TEST_TYPE -k window_function_test
 
         ## Run cache tests
         if [[ "$IS_SPARK_311_OR_LATER" -eq "1" ]]; then
