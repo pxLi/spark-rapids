4d62151847eee513f110559b251ef41a72c3bc1a
Peixin Li <pxli@nyu.edu>
2022-06-28 09:01:49 +0800

    Make pytest report path configurable
    Signed-off-by: Peixin Li <pxli@nyu.edu>

--- integration_tests/run_pyspark_from_build.sh
+++ integration_tests/run_pyspark_from_build.sh
@@ -173,7 +173,7 @@ else
           $TEST_TYPE_PARAM
           "$TEST_ARGS"
           $RUN_TEST_PARAMS
-          --junitxml=TEST-pytest-`date +%s%N`.xml
+          --junitxml=${INPUT_PATH}/TEST-pytest-`date +%s%N`.xml
           "$@")
 
     NUM_LOCAL_EXECS=${NUM_LOCAL_EXECS:-0}
