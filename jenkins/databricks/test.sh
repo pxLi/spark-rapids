3ec671ccd0e54716606992c04d423329ab115aa3
Peixin Li <pxli@nyu.edu>
2022-07-19 15:46:40 +0800

    test only

--- jenkins/databricks/test.sh
+++ jenkins/databricks/test.sh
@@ -92,16 +92,16 @@ mkdir -p /tmp/spark-events
 ## limit parallelism to avoid OOM kill
 export TEST_PARALLEL=4
 if [ -d "$LOCAL_JAR_PATH" ]; then
-    if [[ $TEST_MODE == "DEFAULT" ]]; then
-        ## Run tests with jars in the LOCAL_JAR_PATH dir downloading from the dependency repo
-#        LOCAL_JAR_PATH=$LOCAL_JAR_PATH bash $LOCAL_JAR_PATH/integration_tests/run_pyspark_from_build.sh  --runtime_env="databricks" --test_type=$TEST_TYPE
-
-        ## Run cache tests
-        if [[ "$IS_SPARK_311_OR_LATER" -eq "1" ]]; then
-#          PYSP_TEST_spark_sql_cache_serializer=${PCBS_CONF} \
-#           LOCAL_JAR_PATH=$LOCAL_JAR_PATH bash $LOCAL_JAR_PATH/integration_tests/run_pyspark_from_build.sh  --runtime_env="databricks" --test_type=$TEST_TYPE -k cache_test
-        fi
-    fi
+#    if [[ $TEST_MODE == "DEFAULT" ]]; then
+#        ## Run tests with jars in the LOCAL_JAR_PATH dir downloading from the dependency repo
+##        LOCAL_JAR_PATH=$LOCAL_JAR_PATH bash $LOCAL_JAR_PATH/integration_tests/run_pyspark_from_build.sh  --runtime_env="databricks" --test_type=$TEST_TYPE
+#
+#        ## Run cache tests
+##        if [[ "$IS_SPARK_311_OR_LATER" -eq "1" ]]; then
+##          PYSP_TEST_spark_sql_cache_serializer=${PCBS_CONF} \
+##           LOCAL_JAR_PATH=$LOCAL_JAR_PATH bash $LOCAL_JAR_PATH/integration_tests/run_pyspark_from_build.sh  --runtime_env="databricks" --test_type=$TEST_TYPE -k cache_test
+##        fi
+#    fi
 
     if [[ "$TEST_MODE" == "CUDF_UDF_ONLY" ]]; then
         ## Run cudf-udf tests
@@ -116,16 +116,16 @@ if [ -d "$LOCAL_JAR_PATH" ]; then
             bash $LOCAL_JAR_PATH/integration_tests/run_pyspark_from_build.sh --runtime_env="databricks" -m iceberg --iceberg --test_type=$TEST_TYPE
     fi
 else
-    if [[ $TEST_MODE == "DEFAULT" ]]; then
-        ## Run tests with jars building from the spark-rapids source code
-#        bash /home/ubuntu/spark-rapids/integration_tests/run_pyspark_from_build.sh --runtime_env="databricks" --test_type=$TEST_TYPE
-
-        ## Run cache tests
-        if [[ "$IS_SPARK_311_OR_LATER" -eq "1" ]]; then
-#            PYSP_TEST_spark_sql_cache_serializer=${PCBS_CONF} \
-#            bash /home/ubuntu/spark-rapids/integration_tests/run_pyspark_from_build.sh --runtime_env="databricks" --test_type=$TEST_TYPE -k cache_test
-        fi
-    fi
+#    if [[ $TEST_MODE == "DEFAULT" ]]; then
+#        ## Run tests with jars building from the spark-rapids source code
+##        bash /home/ubuntu/spark-rapids/integration_tests/run_pyspark_from_build.sh --runtime_env="databricks" --test_type=$TEST_TYPE
+#
+#        ## Run cache tests
+#        if [[ "$IS_SPARK_311_OR_LATER" -eq "1" ]]; then
+##            PYSP_TEST_spark_sql_cache_serializer=${PCBS_CONF} \
+##            bash /home/ubuntu/spark-rapids/integration_tests/run_pyspark_from_build.sh --runtime_env="databricks" --test_type=$TEST_TYPE -k cache_test
+#        fi
+#    fi
 
     if [[ "$TEST_MODE" == "CUDF_UDF_ONLY" ]]; then
         ## Run cudf-udf tests
