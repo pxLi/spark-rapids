d6804e8ba6ae9de7852a6802b86d63bf0376f82c
Peixin Li <pxli@nyu.edu>
2023-12-04 17:03:01 +0800

    dynamical pythonpath

--- jenkins/spark-tests.sh
+++ jenkins/spark-tests.sh
@@ -115,7 +115,7 @@ TMP_PYTHON=/tmp/$(date +"%Y%m%d")
 rm -rf $TMP_PYTHON && mkdir -p $TMP_PYTHON && cp -r $SPARK_HOME/python $TMP_PYTHON
 # Get the correct py4j file.
 PY4J_FILE=$(find $TMP_PYTHON/python/lib -type f -iname "py4j*.zip")
-export PYTHONPATH=$TMP_PYTHON/python:$TMP_PYTHON/python/pyspark/:$PY4J_FILE
+export PYTHONPATH=$TMP_PYTHON/python:$PY4J_FILE
 
 # Extract 'value' from conda config string 'key: value'
 CONDA_ROOT=`conda config --show root_prefix | cut -d ' ' -f2`
